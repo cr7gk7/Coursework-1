3181f2cc4203ef38b5812dce7f98bd664587dbd4
cr7gk7 <93276093+cr7gk7@users.noreply.github.com>
2021-11-19 12:29:41 +0000

    Coursework 1

---
+++ products.js
@@ -0,0 +1,101 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Math",
+        "location": "Wembley",
+        "price": 20.00,
+        "image": "./images/math.jpg",
+        "availableInventory": 7,
+        "rating": 5
+    },
+    {
+        "id": 1002,
+        "title": "Art and Design",
+        "location": "Sheferds Bush",
+        "price": 13.00,
+        "image": "./images/art.jpg",
+        "availableInventory": 6,
+        "rating": 5
+    },
+    {
+        "id": 1003,
+        "title": "Chemistry",
+        "location": "Watford",
+        "price": 32.00,
+        "image": "./images/chem.jpg",
+        "availableInventory": 9,
+        "rating": 1
+    },
+    {
+        "id": 1004,
+        "title": "Biology",
+        "location": "Manchester",
+        "price": 25.99,
+        "image": "./images/bio.jpg",
+        "availableInventory": 6,
+        "rating": 3
+    },
+    {
+        "id": 1005,
+        "title": "Music",
+        "location": "Hendon",
+        "price": 17.99,
+        "image": "./images/music.jpg",
+        "availableInventory": 12,
+        "rating": 5
+    },
+    {
+        "id": 1006,
+        "title": "Computer Science",
+        "location": "Hendon",
+        "price": 29.00,
+        "image": "./images/comp.jpg",
+        "availableInventory": 8,
+        "rating": 5
+    },
+    {
+        "id": 1007,
+        "title": "Economics",
+        "location": "Kingsbury",
+        "price": 12.89,
+        "image": "./images/eco.jpg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1008,
+        "title": "Environmental Health",
+        "location": "Brent cross",
+        "price": 15.00,
+        "image": "./images/env.jpg",
+        "availableInventory": 9,
+        "rating": 4
+    },
+    {
+        "id": 1009,
+        "title": "Health care",
+        "location": "Edgware",
+        "price": 2.00,
+        "image": "./images/health.jpg",
+        "availableInventory": 15,
+        "rating": 4
+    },
+    {
+        "id": 10010,
+        "title": "Geography",
+        "location": "Finchley",
+        "price": 7.00,
+        "image": "./images/geo.jpg",
+        "availableInventory": 6,
+        "rating": 2
+    },
+    {
+        "id": 10011,
+        "title": "History",
+        "location": "Wilesden green",
+        "price": 9.99,
+        "image": "./images/history.jpg",
+        "availableInventory": 10,
+        "rating": 4
+    },
+]
